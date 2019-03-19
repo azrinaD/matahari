9f418817d312f632b9eb2a9155a533d7622816b5
azrinaD <48429062+azrinaD@users.noreply.github.com>
2019-03-19 12:49:42 +0800

    Update moon.java

--- moon.java
+++ moon.java
@@ -17,7 +17,7 @@ import java.util.StringTokenizer;
 // The tutorial can be found just here on the SSaurel's Blog : 
 // https://www.ssaurel.com/blog/create-a-simple-http-web-server-in-java
 // Each Client Connection will be managed in a dedicated Thread
-public class JavaHTTPServer implements Runnable{ 
+class JavaHTTPServer implements Runnable{ 
 	
 	static final File WEB_ROOT = new File(".");
 	static final String DEFAULT_FILE = "index.html";
